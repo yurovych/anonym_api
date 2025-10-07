7a230f199325523a954249a207ad3a9a2a70e0c7
yurovych <volodiayurovych@gmail.com>
2025-10-07 20:23:10 +0200

    first_push

---
+++ src/sockets/sockets.gateway.ts
@@ -0,0 +1,210 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import type { IsTyping, Message, Participant } from '../types/generalTypes';
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class SocketsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  private waitingQueue: Participant[] = [];
+  private readonly maxRoomSize = 2;
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+
+  async handleDisconnect(client: Socket) {
+    console.log(`Клієнт ${client.id} відключився`);
+    this.waitingQueue = this.waitingQueue.filter(
+        (participant) => participant.socketId !== client.id,
+    );
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      await client.leave(client.data.chatId as string);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      this.server.to(client.data.chatId as string).emit('chat-ended', {
+        uId: client.id,
+      });
+    } catch (err) {
+      console.error(
+          `Помилка при обробці відключення клієнта ${client.id}:`,
+          err,
+      );
+    }
+  }
+
+  @SubscribeMessage('reconnect-to-chat')
+  async handleReconnectToChat(
+      client: Socket,
+      payload: Omit<Participant, 'socketId'>,
+  ) {
+    const { chatId, uId, interlocutorData, userData } = payload;
+
+    const currentParticipant: Participant = {
+      uId,
+      socketId: client.id,
+      userData,
+      interlocutorData,
+    };
+
+    if (chatId) {
+      const room = this.server.sockets.adapter.rooms.get(chatId);
+      const usersInRoom = room ? room.size : 0;
+      if (usersInRoom < this.maxRoomSize) {
+        try {
+          await client.join(chatId);
+          this.notifyRoomSize(chatId);
+        } catch {
+          console.error('Can not join room');
+        }
+      } else {
+        this.waitingQueue.push(currentParticipant);
+        client.emit('waiting-for-match');
+      }
+    } else {
+      this.waitingQueue.push(currentParticipant);
+      client.emit('waiting-for-match');
+    }
+  }
+
+  @SubscribeMessage('find-chat')
+  async handleFindChat(client: Socket, payload: Omit<Participant, 'socketId'>) {
+    const { uId, userData, interlocutorData } = payload;
+    const currentParticipant: Participant = {
+      uId,
+      socketId: client.id,
+      userData,
+      interlocutorData,
+    };
+
+    const match = this.findMatch(currentParticipant);
+
+    if (match) {
+      try {
+        const chatId = this.generateChatId(uId, match.uId);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        client.data.chatId = chatId;
+        const matchedSocket = this.server.sockets.sockets.get(match.socketId);
+        if (matchedSocket) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          matchedSocket.data.chatId = chatId;
+        }
+
+        const room = this.server.sockets.adapter.rooms.get(chatId);
+        const usersInRoom = room ? room.size : 0;
+
+        if (usersInRoom < this.maxRoomSize) {
+          await client.join(chatId);
+          await this.server.sockets.sockets.get(match.socketId)?.join(chatId);
+
+          this.waitingQueue = this.waitingQueue.filter(
+              (participant) =>
+                  participant.uId !== uId && participant.uId !== match.uId,
+          );
+
+          this.server.to(chatId).emit('chat-created', {
+            chatId,
+            seekerId: uId,
+            matchId: match.uId,
+          });
+
+          this.notifyRoomSize(chatId);
+        } else {
+          this.waitingQueue.push(currentParticipant);
+          client.emit('waiting-for-match');
+        }
+      } catch {
+        console.error('Can not join room');
+      }
+    } else {
+      this.waitingQueue.push(currentParticipant);
+      client.emit('waiting-for-match');
+    }
+  }
+
+  @SubscribeMessage('send-message')
+  handleMessage(client: Socket, payload: Message) {
+    const { chatId, uId, message, createdAt } = payload;
+
+    this.server.to(chatId).emit('receive-message', {
+      uId: uId,
+      message: message,
+      createdAt: createdAt,
+      chatId: chatId,
+      pending: false,
+    });
+  }
+
+  @SubscribeMessage('is-typing')
+  handleTyping(client: Socket, payload: IsTyping): void {
+    const { uId, isTyping, chatId } = payload;
+
+    this.server.to(chatId).emit('user-typing', {
+      uId: uId,
+      isTyping: isTyping,
+    });
+  }
+
+  @SubscribeMessage('leave-chat')
+  async handleLeaveChat(
+      client: Socket,
+      payload: { uId: string; chatId: string },
+  ) {
+    const { uId, chatId } = payload;
+    try {
+      await client.leave(chatId);
+
+      this.waitingQueue = this.waitingQueue.filter(
+          (participant) => participant.uId !== uId,
+      );
+
+      this.server.to(chatId).emit('chat-ended', {
+        uId: uId,
+      });
+    } catch (err) {
+      console.error(`Помилка при виході клієнта з кімнати ${chatId}:`, err);
+    }
+  }
+
+  private notifyRoomSize(chatId: string): void {
+    const room = this.server.sockets.adapter.rooms.get(chatId);
+    const usersInRoom = room ? room.size : 0;
+    this.server.to(chatId).emit('room-size', { usersInRoom });
+  }
+
+  private findMatch(currentParticipant: Participant): Participant | null {
+    return (
+        this.waitingQueue.find((participant) => {
+          return (
+              participant.uId !== currentParticipant.uId &&
+              participant.userData.sex ===
+              currentParticipant.interlocutorData.sex &&
+              currentParticipant.userData.sex ===
+              participant.interlocutorData.sex &&
+              participant.userData.age >=
+              currentParticipant.interlocutorData.ageFrom &&
+              participant.userData.age <=
+              currentParticipant.interlocutorData.ageTo &&
+              currentParticipant.userData.age >=
+              participant.interlocutorData.ageFrom &&
+              currentParticipant.userData.age <=
+              participant.interlocutorData.ageTo &&
+              !participant.userData.blackList.includes(currentParticipant.uId) &&
+              !currentParticipant.userData.blackList.includes(participant.uId)
+          );
+        }) || null
+    );
+  }
+
+  private generateChatId(userId1: string, userId2: string): string {
+    const ids = [userId1, userId2].sort();
+    return `${ids[0]}_${ids[1]}`;
+  }
+}
---
+++ src/types/generalTypes.ts
@@ -0,0 +1,35 @@
+export interface Message {
+  uId: string;
+  message: string;
+  createdAt: number;
+  pending: boolean;
+  chatId: string;
+  userData: { age: number; sex: 'male' | 'female' };
+  interlocutorData: {
+    ageFrom: number;
+    ageTo: number;
+    sex: string;
+  };
+}
+
+export interface IsTyping {
+  uId: string;
+  isTyping: boolean;
+  chatId: string;
+}
+
+export interface Participant {
+  uId: string;
+  socketId: string;
+  userData: {
+    age: number;
+    sex: 'male' | 'female';
+    blackList: string[];
+  };
+  interlocutorData: {
+    ageFrom: number;
+    ageTo: number;
+    sex: string;
+  };
+  chatId?: string;
+}
